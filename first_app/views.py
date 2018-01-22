fafc30dd11d824e8d7398a0b1c092f08a13ee4a0
Jamel Travis <jameltravis@gmail.com>
2018-01-22 01:21:29 -0500

    test view

--- first_app/views.py
+++ first_app/views.py
@@ -1,10 +1,12 @@
+"""Views for our first_app"""
+
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
     """Returns hello world"""
-    my_dictionary = {"insert_me": "Hello I am from views.py"}
-    return render(request, "index.html", context=my_dictionary)
+    my_dictionary = {"insert_me": "Hello I am from views.py!!"}
+    return render(request, "first_app/index.html", context=my_dictionary)
