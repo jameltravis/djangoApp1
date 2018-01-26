d598a9c71d38e69c1cdf0edbe5b245bf7a3454c7
Jamel Travis <jameltravis@gmail.com>
2018-01-25 23:52:53 -0500

    worked on model based views

--- first_app/views.py
+++ first_app/views.py
@@ -2,11 +2,13 @@
 
 from django.shortcuts import render
 # from django.http import HttpResponse
+from first_app.models import *
 
 # Create your views here.
 
 
 def index(request):
     """Returns hello world"""
-    my_dictionary = {"insert_me": "Hello I am from views.py!!"}
-    return render(request, "first_app/index.html", context=my_dictionary)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, "first_app/index.html", context=date_dict)
